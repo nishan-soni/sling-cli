c7adcad8780dcf6831f2825e17dccc7015f20130
Nishan Soni <nishan.soni2@gmail.com>
2025-03-19 13:10:34 -0600

    formatting

--- core/dbio/database/database_snowflake.go
+++ core/dbio/database/database_snowflake.go
@@ -95,13 +95,11 @@ func (conn *SnowflakeConn) Init() error {
 	}
 
 	if internalStage := conn.GetProp("internal_stage"); internalStage != "" {
-
 		snowflakeStage := Table{
 			Schema:  conn.GetProp("schema"),
 			Name:    internalStage,
 			Dialect: dbio.TypeDbSnowflake,
 		}
-
 		conn.SetProp("internal_stage", snowflakeStage.FullName())
 	}
 
